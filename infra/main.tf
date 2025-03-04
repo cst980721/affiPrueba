b3c85891c6a21b05aa12945320b326896bd0dae5
csalazar2 <csalazar@vortexbird.com>
2025-03-04 14:22:22 -0500

    Se agrega archivo index y se ajusta pipeline

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "MiGrupoDeRecursos"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "mistorageaccount123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "mi-container"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "index_html" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.container.name
+  type                   = "Block"
+  source                 = "index.html"
+}
